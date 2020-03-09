2ff30a3864f8b5ba0ba6092a25dae804795e0853
Vasilek <morphizm@ya.ru>
2020-03-09 22:29:00 +0300

    tag

---
+++ chapter2/2.74.rkt
@@ -0,0 +1,23 @@
+#lang racket/base
+
+(require "func.rkt")
+(require rackunit)
+
+; put <on> <type> <element>
+; get <on> <type>
+
+; persons --- index name
+; maybe have some tags such as address, salary
+
+(define (get-record person file-of-persons)
+  ; 
+)
+
+(define (get-salary person file-of-persons)
+  ;
+)
+
+(define (find-employee-record person file-of-persons)
+  ;
+)
+
